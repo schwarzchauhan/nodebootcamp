c83c07624589f3d6ab8ed832e200c25f1a3df766
usernameharshchauhan <harshchauhan0994@gmail.com>
2021-10-28 11:08:31 +0530

    asyn read write file

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+// requiring module -----------
 const fs = require('fs');
 
 console.log('hello');
 
-const t = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(t);
-
-const o = `This is what we know about the avocado : ${t}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', o);
-console.log('file written');
+// non-blocking async. way, -- callbk hell
+// https://nodejs.org/api/fs.html#fsreadfilepath-options-callback
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) throw err;
+    console.log(data1);
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        if (err) throw err;
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+            if (err) throw err;
+            console.log(data3);
+            // https://nodejs.org/api/fs.html#fswritefilefile-data-options-callback
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf8', (err) => {
+                if (err) throw err;
+                console.log('The file has been saved!');
+            });
+        });
+    });
+});
+console.log("some log");
