3f7da39ad94f3c89b7801cc7e41efedfadbfdced
usernameharshchauhan <harshchauhan0994@gmail.com>
2021-10-28 12:04:03 +0530

    url module , routes on browswer

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 // requiring module -----------
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-        data: 'Hello World!',
-        no: 890,
-        name: 'harsh chauhan'
-    }));
+    console.log(req.url);
+    const pathName = req.url;
+
+    if (pathName === '/overview') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({
+            data: 'Hello World!',
+            no: 890,
+            name: 'harsh chauhan'
+        }));
+    } else if (pathName === '/product') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({
+            data: 'path is product'
+        }));
+
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({
+            data: 'path could not be found'
+        }));
+
+    }
 });
 
 // https://nodejs.org/api/http.html#serverlisten
