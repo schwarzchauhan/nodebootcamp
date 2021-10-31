71425e0fded1e4f2b6c9062b5dc1473d330bc4a7
usernameharshchauhan <harshchauhan0994@gmail.com>
2021-10-31 11:00:00 +0530

    2 event2 events module

--- 2-how-node-works/event.js
+++ 2-how-node-works/event.js
@@ -1,7 +1,13 @@
 const EventEmitter = require('events');
+const http = require('http');
 
-const myEmitter = new EventEmitter();
+class Sales extends EventEmitter {
+    constructor() {
+        super();
+    }
+}
 
+const myEmitter = new Sales();
 
 myEmitter.on('newSale', () => {
     console.log('There was  a new sale!');
@@ -16,3 +22,25 @@ myEmitter.on('newSale', stock => {
 });
 
 myEmitter.emit("newSale", 9);
+
+
+/////////////////
+
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    console.log('Request received');
+    console.log(req.url);
+    res.end('request received');
+});
+server.on('request', (req, res) => {
+    console.log('another request received');
+});
+server.on('close', (req, res) => {
+    res.end('server closed');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('waiting for request');
+})
